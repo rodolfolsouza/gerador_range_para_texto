8fb4740b491a4e107947192a626b2fd0c65df4a2
rodolfolsouza <63168467+rodolfolsouza@users.noreply.github.com>
2023-05-01 16:24:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,5 +52,11 @@ if st.button:
             st.text(resultado)
 
                 
-                
-    
+hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
